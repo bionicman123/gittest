1d0bdfab809e65bc55aef0d063b2a5d3679a9d59
bionicman123 <urmas460@hot.ee>
2015-09-28 10:40:28 +0300

    1st commit
    1st commit by bionic

---
+++ src/org/tamm/gittest/GitTest.java
@@ -0,0 +1,14 @@
+package org.tamm.gittest;
+
+public class GitTest {
+
+	public GitTest() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Gittest by bionicman123");
+	}
+
+}
